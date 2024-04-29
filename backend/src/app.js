ca29a1ae45a117f43f3cad30951e20313d9f6993
Akshat Tambi <akshat@Akshats-MacBook-Air.local>
2024-04-29 18:38:43 +0530

    Added exam COntroller, edited exam model, added exam routes, added extra function to cloudinary for delete operations

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace YOUR_FRONTEND_PORT with the port of your frontend application
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
