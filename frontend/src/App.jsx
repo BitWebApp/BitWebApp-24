633c0a5359f6d9b7cacb8c32e7dbda86a9085f54
Hritabhash1 <hritabhashray@gmail.com>
2024-05-10 18:05:06 +0530

    form created and sidebar header updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Layout from './components/Layout'
-import Dashboard from './components/Dashboard'
-import Login from './components/Login'
-import Signup from './components/signup'
-import Sidebar from './components/Sidebar';
-import Loginadmin from './components/Loginadmin';
-import Signupadmin from './components/Signupadmin';
-import LandingPage from './components/LandingPage';
-import Userform from './components/userform';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Signup from "./components/signup"; // Make sure to import the correct file path
+import Userform from "./components/userform"; // Import the Userform component
+import Sidebar from "./components/Sidebar";
+import Loginadmin from "./components/Loginadmin";
+import Signupadmin from "./components/Signupadmin";
+import LandingPage from "./components/LandingPage";
+import Academicform from "./components/academicform";
+import Awardform from "./components/award-form";
 export default function App() {
   return (
     <div>
       <Router>
         <Routes>
-        <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/log" element={<Login />} />
-          <Route path="/db" element={<Layout />} >
-          
+          <Route path="/db" element={<Layout />}>
             <Route index element={<Dashboard />} />
+            <Route path="user-form" element={<Userform />} /> 
+            <Route path="academic-form" element={<Academicform />} /> 
+            <Route path="award-form" element={<Awardform />} /> 
 
           </Route>
+        
           <Route path="/sg" element={<Signup />} />
           <Route path="/sb" element={<Sidebar />} />
           <Route path="/log.a" element={<Loginadmin />} />
           <Route path="/sg.a" element={<Signupadmin />} />
-          <Route path="/u-form" element={<Userform />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
