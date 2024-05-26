a3be0dcd047e9d9eadda2062a679093d524f34e6
sumitt-kun <sumitkumar933434@gmail.com>
2024-05-27 01:33:59 +0530

    add auth middlewares for admins

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,8 +2,9 @@ import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
+import { Admin } from "../models/admin.model.js";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
+const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
@@ -25,3 +26,30 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, error?.message || "Invalid Access Token");
   }
 });
+
+const verifyAdmin = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request!");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken.isAdmin) {
+      throw new ApiError(403, "Access forbidden: Admins only");
+    }
+    const admin = await Admin.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!admin) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.admin = admin;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
+
+export { verifyAdmin, verifyJWT };
