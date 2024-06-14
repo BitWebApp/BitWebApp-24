810c05451ca601c368303f348574ff93effd262b
Gravity-528 <btech10086.22@bitmesra.ac.in>
2024-06-14 19:13:43 +0530

    added project

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,86 +1,3 @@
-
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import { ApiError } from "../utils/ApiError.js";
-// import { Project } from "../models/project.model.js";
-// import { uploadOnCloudinary } from "../utils/Cloudinary.js";
-// import { ApiResponse } from "../utils/ApiResponse.js";
-// import { verifyJWT } from "../middlewares/auth.middleware.js";
-// import { upload } from "../middlewares/multer.middleware.js";
-// import { User } from "../models/user.model.js";
-
-// const createProject = asyncHandler(async (req, res) => {
-//   const { projectName, domain, projectLink, techStack, guide } = req.body;
-//   const {_id: userid } = req.user; // Assuming user_id is obtained from the JWT token
-//   console.log("user is ",userid);
-//   // Input validation
-//   if (!projectName || !domain || !techStack || !guide || !projectLink) {
-//     throw new ApiError(400, "All fields are required");
-//   }
-
-//   // Check if project already exists if yes then append kr do apna contribution :)
-//   const existingProject = await Project.findOne({ projectName });
-//   if (existingProject) {
-    
-//     await Project.findByIdAndUpdate(existingProject._id, {
-//       $addToSet: { name: userid },
-//     });
-//     return res.status(200).json(
-//       new ApiResponse(
-//         200,
-//         existingProject,
-//         "Project already exists. Your contribution has been appended."
-//       )
-//     );
-//   }
-
-  
-//   const projectLocalPath = req.files?.projectId[0]?.path;
-//   if (!projectLocalPath) {
-//     throw new ApiError(400, "Project file is required");
-//   }
-//   const projectId = await uploadOnCloudinary(projectLocalPath);
-
-  
-//   const newProject = await Project.create({
-//     projectName,
-//     domain,
-//     techStack,
-//     guide,
-//     projectLink,
-//     doc: projectId.url,
-//   });
-
-//   await Project.findByIdAndUpdate(newProject._id,{
-//     $addToSet:{name:userid}
-//   })
-
-//   const findUser=await User.findById(userid);
-
-//   await User.findByIdAndUpdate(findUser._id,{
-//     $addToSet:{proj:newProject._id}
-//   })
-
-//   return res.status(201).json(
-//     new ApiResponse(201, {newProject,findUser}, "Project created successfully")
-//   );
-// });
-
-// const ShowProject=asyncHandler(async(req,res)=>{
-//     const showData=await Project.find();
-
-//     res.status(200).json(
-//         new ApiResponse(
-//             200,
-//             showData,
-//             "data shown succesfully"
-//         )
-//     )
-// })
-
-// export { createProject,ShowProject };
-
-    
-
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Project } from "../models/project.model.js";
@@ -159,9 +76,11 @@ const ShowProject = asyncHandler(async (req, res) => {
 });
 
 const deleteProject = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  // const { id } = req.params;
+  // const id=req.user;
+  const { _id: userId } = req.user;
   try {
-    const project = await Project.findById(id);
+    const project = await Project.findById(userId);
     if (!project) {
       return res.status(404).json(
         new ApiResponse(404, null, "Project not found")
@@ -186,11 +105,13 @@ const deleteProject = asyncHandler(async (req, res) => {
 });
 
 const editProject = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  // const { id } = req.params;
+  // const id=req.user;
+  const { _id: userId } = req.user;
   const { projectName, domain, projectLink, techStack, guide } = req.body;
 
   try {
-    const project = await Project.findById(id);
+    const project = await Project.findById(userId);
     if (!project) {
       throw new ApiError(404, "Project not found");
     }
@@ -235,15 +156,24 @@ const editProject = asyncHandler(async (req, res) => {
 });
 
 const showProjectById=asyncHandler(async(req,res)=>{
-  const {id}=req.params
+  // const {id}=req.params
+  // const id=req.user
+  const { _id: userId } = req.user;
+  try{
 
-  const FindProj=await findById(id);
+  const FindProj=await User.findById(userId).populate('proj');
+
   if(!FindProj){
     throw new ApiError(404,"some error in fetching data");
   }
+  
   res.status(200).json(
-    new ApiResponse(200,FindProj,"Project fetched successfully")
+    new ApiResponse(200,FindProj.proj,"Project fetched successfully")
   )
+}catch(err){
+  console.log(err);
+   throw new ApiError(501,"some error in fetch project by id");
+}
 })
 
 export { createProject, ShowProject, deleteProject, editProject,showProjectById };
