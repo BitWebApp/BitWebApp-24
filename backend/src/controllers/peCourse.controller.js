f50e0ed07faa3824c862f12ef17aecbad39e92d4
Akshat Tambi <1akshat.tambi@gmail.com>
2024-10-08 17:02:10 +0530

    backend for PE- update

--- backend/src/controllers/peCourse.controller.js
+++ backend/src/controllers/peCourse.controller.js
@@ -2,30 +2,36 @@ import { PeCourse } from '../models/peCourse.model.js';
 import { User } from '../models/user.model.js';
 
 export const addPeCourse = async (req, res) => 
-{
-    try 
     {
-        const { peCourseId } = req.body;
-        const userId = req.user._id; 
-        const course = await PeCourse.findOne({ courseCode: peCourseId.toString() });
-        if (!course) {
-            return res.status(404).json({ success: false, message: 'PE Course not found!' });
+        try 
+        {
+            const { peCourseId } = req.body;
+            const userId = req.user._id; 
+            const course = await PeCourse.findOne({ courseCode: peCourseId.toString() });
+    
+            if (!course) {
+                return res.status(404).json({ success: false, message: 'PE Course not found!' });
+            }
+            const user = await User.findById(userId);
+            if (user.branch !== course.branch) {
+                return res.status(400).json({ success: false, message: 'You cannot select a PE course from a different branch.' });
+            }
+            const isStudentAlreadyEnrolled = user.peCourses?.includes(peCourseId.toString());
+            if (isStudentAlreadyEnrolled) {
+                return res.status(400).json({ success: false, message: 'You have already selected a PE course.' });
+            }
+            course.students.push(userId);
+            await course.save();
+    
+            await User.findByIdAndUpdate(userId, {
+                $push: { peCourses: course._id },
+            });
+    
+            res.status(200).json({ success: true, message: 'PE course added successfully.' });
+        } catch (error) {
+            res.status(500).json({ success: false, message: 'Server error, try again later.' });
         }
-        const user = await User.findById(userId);
-        const isStudentAlreadyEnrolled = user.peCourses?.includes(peCourseId.toString());
-        if (isStudentAlreadyEnrolled) {
-            return res.status(400).json({ success: false, message: 'You have already selected a PE course.' });
-        }
-        course.students.push(userId);
-        await course.save();
-        await User.findByIdAndUpdate(userId, {
-            $push: { peCourses: course._id },
-        });
-        res.status(200).json({ success: true, message: 'PE course added successfully.' });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Server error, try again later.' });
-    }
-};
+    };
 
 export const getAllPeCourses = async (req, res) => {
   try {
