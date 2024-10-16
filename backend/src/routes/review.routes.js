a1d76eb83a7e4d8f0d72a92152b39c34cb646d9e
sumitt-kun <sumitkumar933434@gmail.com>
2024-10-16 16:47:55 +0530

    updated review section

--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { 
-  addReview, 
-  getReviews 
-} from '../controllers/review.controller.js';
-import { verifyAdmin } from "../middlewares/auth.middleware.js";
+import { addReview, getReviews } from "../controllers/review.controller.js";
+import { verifyAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route('/reviews').post(addReview)
-router.route('/reviews').get(verifyAdmin, getReviews)
+router.route("/reviews").post(verifyJWT, addReview);
+router.route("/reviews").get(verifyAdmin, getReviews);
 
 export default router;
