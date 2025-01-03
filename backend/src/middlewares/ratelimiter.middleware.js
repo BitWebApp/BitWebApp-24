38b19d4273e80f293c4ccadb6dcfa3dbda03477f
sumitt-kun <sumitkumar933434@gmail.com>
2025-01-03 23:04:49 +0530

    updated rate limiter middleware

--- backend/src/middlewares/ratelimiter.middleware.js
+++ backend/src/middlewares/ratelimiter.middleware.js
@@ -3,9 +3,9 @@ import rateLimit from "express-rate-limit";
 const createRateLimiter = (options) => {
   return rateLimit({
     windowMs: options.windowMs || 15 * 60 * 1000,
-    max: options.max || 100,
-    message: options.message || "Too many requests, please try again later.",
-    headers: options.headers !== undefined ? options.headers : true,
+    limit: options.max || 100,
+    standardHeaders: true,
+    legacyHeaders: false,
   });
 };
 
