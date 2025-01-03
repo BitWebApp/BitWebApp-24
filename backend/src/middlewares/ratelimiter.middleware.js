1f2d90a073f0af74f8f8c1a7813f6c9f656dcf32
sumitt-kun <sumitkumar933434@gmail.com>
2025-01-04 00:08:37 +0530

    .

--- backend/src/middlewares/ratelimiter.middleware.js
+++ backend/src/middlewares/ratelimiter.middleware.js
@@ -1,10 +1,13 @@
 import rateLimit from "express-rate-limit";
+import requestIp from "request-ip";
+
+export const requestIpMiddleware = requestIp.mw();
 
 export const createRateLimiter = ({ windowMs, max }) => {
   return rateLimit({
     windowMs,
     max,
-    keyGenerator: (req) => req.ip,
+    keyGenerator: (req) => req.clientIp,
     standardHeaders: true,
     legacyHeaders: false,
   });
--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { addReview, getReviews } from "../controllers/review.controller.js";
 import { verifyAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
-import { createRateLimiter } from "../middlewares/ratelimiter.middleware.js";
+import {
+  createRateLimiter,
+  requestIpMiddleware,
+} from "../middlewares/ratelimiter.middleware.js";
 
 const router = Router();
 
 const reviewLimiter = createRateLimiter({ windowMs: 15 * 60 * 1000, max: 5 });
 
-router.route("/reviews").post(reviewLimiter, addReview);
+router.route("/reviews").post(requestIpMiddleware, reviewLimiter, addReview);
 router.route("/reviews").get(verifyAdmin, getReviews);
 
 export default router;
