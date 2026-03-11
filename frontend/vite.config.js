f7e19d66d06ec4cfec70317dce75b1081933a849
Suryansh Dey <deysuryansh@gmail.com>
2026-03-11 23:43:17 +0530

    Adding live public folder server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import sirv from 'sirv'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +13,20 @@ export default defineConfig({
       "/api": "http://localhost:8000",
     },
   },
-  plugins: [react()],
+  plugins: [react(),
+    {
+      name: 'serve-live-public-in-preview',
+      configurePreviewServer(server) {
+        // This serves the LIVE 'public' folder at the root level
+        server.middlewares.use(
+          sirv(resolve(__dirname, 'public'), {
+            dev: true,
+            etag: false,
+            extensions: [] 
+          })
+        )
+      }
+    }],
   preview: {
     host: true,
     port: 3000,
